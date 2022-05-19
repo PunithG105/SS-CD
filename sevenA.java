4dfeebc57690d1d7c8cd3a8b158583abda0b8e13
Punith G <91651608+PunithG105@users.noreply.github.com>
2022-05-19 10:08:20 +0530

    Update sevenA.java

--- sevenA.java
+++ sevenA.java
@@ -1,41 +1,31 @@
-import java.util.*;
-public class nineA{
+import java.util.Scanner;
+public class sevenA{
 	public static void main(String[] args){
-		int n,m,i,j,hit=0,k=0,l=0;
+		int n,q,t=0,total;
 		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter the number of inputs");
+		System.out.println("Enter the no of processes");
 		n=sc.nextInt();
-		int[] a=new int[n];
-		for(i=0;i<n;i++){
-			System.out.print("a["+i+"] : ");
-			a[i]=sc.nextInt();
-			System.out.println();
+		int[] bt=new int[n];
+		for(int i=0;i<n;i++){
+			System.out.println("Enter burst time for process "+i);
+			bt[i]=sc.nextInt();
+			t+=bt[i];		
 		}
-		System.out.println("Enter the frame size");
-		m=sc.nextInt();
-		int[][] frame=new int[m][2];
-		for(i=0;i<m;i++){
-			frame[i][0]=-1;
-			frame[i][1]=-1;
-		}
-		System.out.println("============================");
-		for(i=0;i<n;i++){
-			for(j=0,l=0;j<m&&a[i]!=frame[j][0];j++)
-				if(frame[l][1]>frame[j][1])
-					l=j;
-			if(j==m){
-				frame[l][0]=a[i];
-				frame[l][1]=i;
-				System.out.println(frame[0][0]+"("+frame[0][1]+")"+" \t "+frame[1][0]+"("+frame[1][1]+")"+" \t "+frame[2][0]+"("+frame[2][1]+")");
+		total=t;
+		System.out.println("Enter the time quanta");
+		q=sc.nextInt();
+		for(int i=0,c=0;t>0;i=(i+1)%n){
+			if(bt[i]==0)
+				continue;
+			else if(bt[i]<q){
+				c=bt[i];bt[i]=0;	
 			}
 			else{
-				hit++;
-				frame[j][1]=i;
-				System.out.println(frame[0][0]+"("+frame[0][1]+")"+" \t "+frame[1][0]+"("+frame[1][1]+")"+" \t "+frame[2][0]+"("+frame[2][1]+")"+" \t Hit");
+				c=q;bt[i]-=q;
 			}
-			System.out.println("============================");
+			System.out.print((total-t)+"|");
+			t-=c;
+			System.out.println("P"+i+"|"+(total-t)+"|");
 		}
-		System.out.println("Hits : "+hit+", Faults : "+(n-hit));
-		sc.close();
 	}
 }
